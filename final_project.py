c5766f05e700a35ad349f05e2895f9da6997c211
Jamie Kim <jamiekim@Jamies-MBP.ycdsb.ca>
2019-05-30 14:27:26 -0400

    fix

--- final_project.py
+++ final_project.py
@@ -3,25 +3,59 @@ import arcade
 import math
 import os
 
-SPRITE_SCALING_SEAL = 0.2
+SPRITE_SCALING_PLAYER = 0.4
 SPRITE_SCALING_SHRIMPS = 0.2
 SPRITE_SCALING_TRASH = 0.1
 
-SHRIMPS_COUNT = 30
-TRASH_COUNT = 15
+SHRIMPS_COUNT = 35
+TRASH_COUNT = 10
 
 SCREEN_WIDTH = 1250
 SCREEN_HEIGHT = 800
 
-class Seal(arcade.Sprite):
+class Shrimps(arcade.Sprite):
+
     def __init__(self, filename, sprite_scaling):
+        """ Constructor. """
+        # Call the parent class (Sprite) constructor
+        super().__init__(filename, sprite_scaling)
+
+        # Current angle in radians
+        self.circle_angle = 0
+
+        # How far away from the center to orbit, in pixels
+        self.circle_radius = 0
+
+        # How fast to orbit, in radians per frame
+        self.circle_speed = 0.008
+
+        # Set the center of the point we will orbit around
+        self.circle_center_x = 0
+        self.circle_center_y = 0
+
+    def update(self):
+        """ Update the ball's position. """
+        # Calculate a new x, y
+        self.center_x = self.circle_radius * math.sin(self.circle_angle) \
+                        + self.circle_center_x
+        self.center_y = self.circle_radius * math.cos(self.circle_angle) \
+                        + self.circle_center_y
+
+        # Increase the angle in prep for the next round.
+        self.circle_angle += self.circle_speed
+
+class Trash(arcade.Sprite):
+
+    def __init__(self, filename, sprite_scaling):
+
         super().__init__(filename, sprite_scaling)
 
         self.change_x = 0
         self.change_y = 0
 
     def update(self):
-        # Move the seal
+
+        # Move the fish
         self.center_x += self.change_x
         self.center_y += self.change_y
 
@@ -44,19 +78,13 @@ class MyGame(arcade.Window):
     def __init__(self):
         """ Initializer """
         # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Seal and Shrimps")
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Seals love shrimps ")
 
         # Variables that will hold sprite lists
         self.shrimps_list = None
-        self.seal_list = None
+        self.trash_list = None
 
-        # Set up the player info
-        self.player_sprite = None
-        self.score = 0
-
-        # Don't show the mouse cursor
-        self.set_mouse_visible(False)
-
+        # set the background
         arcade.set_background_color(arcade.color.SKY_BLUE)
 
     def setup(self):
@@ -64,46 +92,59 @@ class MyGame(arcade.Window):
 
         # Sprite lists
         self.shrimps_list = arcade.SpriteList()
-        self.seal_list = arcade.SpriteList()
+        self.trash_list = arcade.SpriteList()
 
-        # Score
-        self.score = 0
+        # Create the shrimps
+        for i in range(SHRIMPS_COUNT):
+            # Create the shrimps instance
+            # shrimp image
+            shrimps = Shrimps("images/shrimps.png", SPRITE_SCALING_SHRIMPS)
 
-        # Set up the player
-        # Character images
-        self.player_sprite = arcade.Sprite("images/seal.png", SPRITE_SCALING_SEAL)
-        self.player_sprite.center_x = 120
-        self.player_sprite.center_y = 120
-        self.seal_list.append(self.player_sprite)
+            # Position the center of the circle the can will orbit
+            shrimps.circle_center_x = random.randrange(SCREEN_WIDTH)
+            shrimps.circle_center_y = random.randrange(SCREEN_HEIGHT)
 
+            # Random radius from 10 to 200
+            shrimps.circle_radius = random.randrange(10, 200)
+
+            # Random start angle from 0 to 2pi
+            shrimps.circle_angle = random.random() * 2 * math.pi
+
+            # Add the can to the lists
+            self.shrimps_list.append(shrimps)
+
+        for i in range(TRASH_COUNT):
+            # Create the trash instance
+            # trash image
+            trash = Trash("images/trash.png", SPRITE_SCALING_TRASH)
+
+            # Position the fish
+            trash.center_x = random.randrange(SCREEN_WIDTH)
+            trash.center_y = random.randrange(SCREEN_HEIGHT)
+            trash.change_x = random.randrange(-3, 4)
+            trash.change_y = random.randrange(-3, 4)
+
+            # Add the trash to the lists
+            self.trash_list.append(trash)
 
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
-        self.seal_list.draw()
+        self.shrimps_list.draw()
+        self.trash_list.draw()
 
+    def update(self, delta_time):
+        if len(self.shrimps_list) > 0:
+            self.shrimps_list.update()
 
-    def on_key_press(self, key, modifiers):
-        # Pull down the apple to the ground
-        if key == arcade.key.UP:
-            self.player_sprite.center_y += 20
-
-        if key == arcade.key.LEFT:
-            self.player_sprite.center_x -= 20
-
-        if key == arcade.key.RIGHT:
-            self.player_sprite.center_x += 20
-
-        if key == arcade.key.DOWN:
-            self.player_sprite.center_y -= 20
-
+        if len(self.trash_list) > 0:
+            self.trash_list.update()
 
 def main():
     """ Main method """
     window = MyGame()
     window.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
