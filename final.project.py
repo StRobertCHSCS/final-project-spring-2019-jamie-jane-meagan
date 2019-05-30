cbdc37b1a529984e5ba8c6c1fe321a4ea673eeb7
Jamie Kim <jamiekim@Jamies-MBP.ycdsb.ca>
2019-05-30 10:28:09 -0400

    moving trash

--- final.project.py
+++ final.project.py
@@ -8,7 +8,7 @@ SPRITE_SCALING_SHRIMPS = 0.2
 SPRITE_SCALING_TRASH = 0.1
 
 SHRIMPS_COUNT = 35
-TRASH_COUNT = 15
+TRASH_COUNT = 10
 
 SCREEN_WIDTH = 1250
 SCREEN_HEIGHT = 800
@@ -44,6 +44,34 @@ class Shrimps(arcade.Sprite):
         # Increase the angle in prep for the next round.
         self.circle_angle += self.circle_speed
 
+class Trash(arcade.Sprite):
+
+    def __init__(self, filename, sprite_scaling):
+
+        super().__init__(filename, sprite_scaling)
+
+        self.change_x = 0
+        self.change_y = 0
+
+    def update(self):
+
+        # Move the fish
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # If we are out-of-bounds, then 'bounce'
+        if self.left < 0:
+            self.change_x *= -1
+
+        if self.right > SCREEN_WIDTH:
+            self.change_x *= -1
+
+        if self.bottom < 0:
+            self.change_y *= -1
+
+        if self.top > SCREEN_HEIGHT:
+            self.change_y *= -1
+
 class MyGame(arcade.Window):
     """ Our custom Window Class"""
 
@@ -54,6 +82,7 @@ class MyGame(arcade.Window):
 
         # Variables that will hold sprite lists
         self.shrimps_list = None
+        self.trash_list = None
 
         # set the background
         arcade.set_background_color(arcade.color.SKY_BLUE)
@@ -63,6 +92,7 @@ class MyGame(arcade.Window):
 
         # Sprite lists
         self.shrimps_list = arcade.SpriteList()
+        self.trash_list = arcade.SpriteList()
 
         # Create the shrimps
         for i in range(SHRIMPS_COUNT):
@@ -83,14 +113,32 @@ class MyGame(arcade.Window):
             # Add the can to the lists
             self.shrimps_list.append(shrimps)
 
+        for i in range(TRASH_COUNT):
+            # Create the trash instance
+            # trash image
+            trash = Trash("images/trash.png", SPRITE_SCALING_TRASH)
+
+            # Position the fish
+            trash.center_x = random.randrange(SCREEN_WIDTH)
+            trash.center_y = random.randrange(SCREEN_HEIGHT)
+            trash.change_x = random.randrange(-3, 4)
+            trash.change_y = random.randrange(-3, 4)
+
+            # Add the trash to the lists
+            self.trash_list.append(trash)
+
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
         self.shrimps_list.draw()
+        self.trash_list.draw()
 
     def update(self, delta_time):
         if len(self.shrimps_list) > 0:
             self.shrimps_list.update()
+
+        if len(self.trash_list) > 0:
+            self.trash_list.update()
 
 def main():
     """ Main method """
